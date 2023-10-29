12375adeb94f0746af76f259d83caf06ce8e5b6e
pinkal <pinkal@hanu.ai>
2023-10-29 14:35:04 +0530

    added all nav pages

--- siteapp/urls.py
+++ siteapp/urls.py
@@ -5,5 +5,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
+    path('services/',views.services,name='services'),
+    path('experience/',views.experience,name='experience'),
+    path('education/',views.education,name='education'),
+    path('skills/',views.skills,name='skills'),
+    path('portfolio/',views.portfolio,name='portfolio'),
+    path('testimonials/',views.testimonials,name='testimonials'),
+    
 ]
--- siteapp/views.py
+++ siteapp/views.py
@@ -1,4 +1,31 @@
 from django.shortcuts import render
 # Create your views here.
-def index(request):
-    return render(request,'siteapp/home.html')
+def contact(request):
+    return render(request,'siteapp/contact.html')
+
+def about(request):
+    return render(request,'siteapp/about.html')
+
+def services(request):
+    return render(request,'siteapp/services.html')
+
+
+def experience(request):
+    return render(request,'siteapp/experience.html')
+
+
+def education(request):
+    return render(request,'siteapp/education.html')
+
+
+def skills(request):
+    return render(request,'siteapp/skills.html')
+
+
+def portfolio(request):
+    return render(request,'siteapp/portfolio.html')
+
+def testimonials(request):
+    return render(request,'siteapp/testimonials.html')
+
+
